37dbcbeef98faaa565f13de227e8a38d9f8c23a5
marclevetin <marc@derbyhat.org>
2017-04-18 16:54:14 -0400

    Preliminary tests added to User model

--- spec/users_spec.rb
+++ spec/users_spec.rb
@@ -1,18 +1,45 @@
 require 'rails_helper'
 
-feature "feature description" do
+feature "Users " do
 
-  context "context description" do
-    let(:symbol_validated_later) { create :symbol_validated_later
-    let!(:symbol_validated_now) { create :symbol_validated_now, field: value }
+  context "Walking through all of the needed CRUD actions" do
 
-    before do
-      visit "path"
+    scenario "New user is added successfully" do
+
+      visit new_user_path
+
+      fill_in "Name", with: "Doug"
+      fill_in "Email", with: "doug@fmail.com"
+      fill_in "Encrypted password", with: "dougrocksthecasbah"
+      fill_in "Role", with: "command dude"
+
+      click_button "Create User"
+
+      expect(page).to have_content("Users Index page")
+      expect(page).to have_content("Doug")
     end
 
-    scenario "scenario description" do
-      expect(page).to have_content("expected text")
-      expect(page).to_not have_content(expected value)
+    scenario "New user is NOT added successfully" do
+      visit new_user_path
+
+      click_button "Create User"
+
+      expect(page).to have_content("User not created.  Try again.")
+    end
+
+    scenario "Index page shows correct content" do
+
+      visit users_path
+
+      expect(page).to have_content("Users Index page")
+    end
+
+    scenario "Show page appears" do
+      sparky = User.create(name:'Sparky', email:'sparky@gmail.com', encrypted_password: 'w', role: 'a')
+      visit user_path(sparky)
+
+      expect(page).to have_content("Show page!")
+
     end
   end
 end
