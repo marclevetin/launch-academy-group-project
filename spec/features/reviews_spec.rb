86c783f81a188f638dbb2416890572aa9168c598
Solomon <sdgoldiamond@gmail.com>
2017-04-19 14:45:40 -0400

    fixing hound styling issues

--- spec/features/reviews_spec.rb
+++ spec/features/reviews_spec.rb
@@ -5,22 +5,19 @@ feature "Reviews" do
   context "Walking through needed CRUD actions" do
 
     scenario "Review page shows correct content" do
-      Sparky = User.create!(name:'Sparky', email:'sparky@gmail.com', encrypted_password: 'w', role: 'a')
+      Sparky = User.create!(name:'Sparky', email:'sparky@gmail.com',
+        encrypted_password: 'w', role: 'a')
 
       visit user_reviews_path(Sparky)
 
       expect(page).to have_content("Reviews Index page")
     end
 
-
-  context "Walking through needed CRUD actions" do
-
     scenario "New Review is added successfully" do
-      Sparky = User.create!(name:'Sparky', email:'sparky@gmail.com', encrypted_password: 'w', role: 'a')
-
+      Sparky = User.create!(name:'Sparky', email:'sparky@gmail.com',
+        encrypted_password: 'w', role: 'a')
 
       visit new_user_review_path(Sparky)
-
 
       fill_in "title", with: "Denver omelette review"
       fill_in "body", with: "This omelette is GOOD!"
@@ -34,15 +31,14 @@ feature "Reviews" do
     end
 
     scenario "New Review is NOT added successfully" do
-      Sparky = User.create!(name:'Sparky', email:'sparky@gmail.com', encrypted_password: 'w', role: 'a')
-
+      Sparky = User.create!(name:'Sparky', email:'sparky@gmail.com',
+        encrypted_password: 'w', role: 'a')
 
       visit new_user_review_path(Sparky)
 
       click_button "Create Review"
 
       expect(page).to have_content("Review not created.  Try again.")
-      end
     end
   end
 end
