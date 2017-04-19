f9b3f00482980f20614171d6aca0b0aa3c2bb5be
jmalochee <jmalochee@gmail.com>
2017-04-19 17:12:18 -0400

    main.js config

--- react/src/main.js
+++ react/src/main.js
@@ -1,6 +1,17 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { browserHistory } from 'react-router';
+
+import routes from './routes';
+import Root from './components/Root';
+
+let reactDOMRender = (element) => {
+  ReactDOM.render(
+    <Root browserHistory={browserHistory} routes={routes} />,
+    element
+  );
+};
 
 $(function() {
   ReactDOM.render(
