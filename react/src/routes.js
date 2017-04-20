cb627516176c0b8a2fe62f33b313c164d269143d
jmalochee <jmalochee@gmail.com>
2017-04-20 17:00:15 -0400

    clean react refactor

---
+++ react/src/routes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+import Layout from './components/Layout';
+
+let routes = (
+  <Route path="/" component={Layout}>
+  </Route>
+);
+
+export default routes;
