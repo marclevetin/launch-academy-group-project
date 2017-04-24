c31502edbb29a598a2e08d638ec9ee14dc0fa285
jmalochee <jmalochee@gmail.com>
2017-04-24 11:35:04 -0400

    index item and container, wip

--- react/src/routes.js
+++ react/src/routes.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-import Layout from './components/Layout';
+import OmeletteIndexContainer from './containers/OmeletteIndexContainer';
 
 let routes = (
-  <Route path="/" component={Layout}>
+  <Route path="/" component={OmeletteIndexContainer}>
+    
   </Route>
 );
 
