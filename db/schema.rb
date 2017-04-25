4441e7dae879da15db8d2f57be932cab7da512e5
jmalochee <jmalochee@gmail.com>
2017-04-25 14:07:40 -0400

    cleaned up schema

--- db/schema.rb
+++ db/schema.rb
@@ -23,16 +23,6 @@ ActiveRecord::Schema.define(version: 20170425140026) do
     t.index ["user_id"], name: "index_omelettes_on_user_id", using: :btree
   end
 
-  create_table "omlettes", force: :cascade do |t|
-    t.string   "title",       null: false
-    t.text     "description", null: false
-    t.string   "ingredients"
-    t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.index ["user_id"], name: "index_omlettes_on_user_id", using: :btree
-  end
-
   create_table "reg_users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
