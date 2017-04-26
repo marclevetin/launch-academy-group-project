b9a82c8c8afb22ac9fbc040060b9fd11791540b4
jmalochee <jmalochee@gmail.com>
2017-04-26 15:46:57 -0400

    review belongs_to omelette

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,5 @@ class Review < ApplicationRecord
   validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than: 6 }
 
   belongs_to :reg_user
+  belongs_to :omelette
 end
