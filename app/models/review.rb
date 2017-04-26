e16b25c2719f805f9d046dc7667cc5fc72c5acd2
jmalochee <jmalochee@gmail.com>
2017-04-26 16:28:34 -0400

    feeding the hound

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,10 @@
 class Review < ApplicationRecord
   validates :body, presence: true
-  validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than: 6 }
+  validates :rating, presence: true,
+    numericality: {
+      only_integer: true, greater_than_or_equal_to: 0,
+      less_than: 6
+    }
 
   belongs_to :reg_user
   belongs_to :omelette
